f2cfecb1b7e555acdc24ae5149a38161ddcf5e2e
Amanda M <amandaseagull3@gmail.com>
2018-04-06 18:37:42 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
-def hello(array)
-  i = 0 
-  collection = []
-  while i < array.length
-  collection << 
-    yield(array[i])
-    i += 1
-  end
-  collection
-end
+# def hello(array)
+#   i = 0 
+#   collection = []
+#   while i < array.length
+#   collection << 
+#     yield(array[i])
+#     i += 1
+#   end
+#   collection
+# end
 
 
 
